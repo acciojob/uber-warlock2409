f9b18624a080210053eef64cd5ae90e31c732ef1
rajaram <rajaram@runloyal.com>
2023-06-24 13:51:49 +0530

    tirst

--- src/main/java/com/driver/model/Admin.java
+++ src/main/java/com/driver/model/Admin.java
@@ -12,7 +12,7 @@ public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int adminId;
-   private String userName;
+   private String username;
 
    private String password;
 
@@ -27,12 +27,12 @@ public class Admin {
         this.adminId = adminId;
     }
 
-    public String getUserName() {
-        return userName;
+    public String getUsername() {
+        return username;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public List<Driver> getDriverList() {
